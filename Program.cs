8a2b7a1c64a3236cbd53436043a09f07951371e5
Gilcemir <gangelofilho@gmail.com>
2022-02-01 08:44:10 -0300

    Upando para o github

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+public class CheckOpenClose
+{
+    public static bool check(string str)
+    {
+        Stack myStack = new Stack();
+        foreach (char c in str)
+        {
+            char x;
+            switch (c)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    myStack.Push (c);
+                    break;
+                case ')':
+                    x = Convert.ToChar(myStack.Pop());
+                    if (x != '(') return false;
+                    break;
+                case '}':
+                    x = Convert.ToChar(myStack.Pop());
+                    if (x != '{') return false;
+                    break;
+                case ']':
+                    x = Convert.ToChar(myStack.Pop());
+                    if (x != '[') return false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (myStack.Count == 0) return true;
+        return false;
+    }
+
+    public static void Main()
+    {
+        List<string> listaTeste = new List<string>();
+        listaTeste.Add("()(){}[]");
+        listaTeste.Add("[)");
+        listaTeste.Add("[((()))]");
+        listaTeste.Add("[}[");
+        listaTeste.Add("");
+        listaTeste.Add("([]())");
+        listaTeste.Add("(a)(b)(c)");
+
+        foreach(string s in listaTeste)
+        {
+            Console.WriteLine($"string = {s} => {check(s)}");
+        }
+        Console.ReadLine();
+    }
+}
